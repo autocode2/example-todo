3192c2af73291cb12dbc13898ce9458d18e9f83d
Gareth Andrew <gingerhendrix@gmail.com>
2024-07-31 00:50:48 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-rm -rf generated
-mkdir generated
 cd generated
-node ../../../auto-code2/packages/auto-code-cli/dist/index.js code -i ../prompt.txt -m ${MODEL}
+node ../../../../auto-code2/packages/auto-code-cli/dist/index.js code -i ../prompt.txt -m ${MODEL} --exclude-from .llmignore -m haiku
 
